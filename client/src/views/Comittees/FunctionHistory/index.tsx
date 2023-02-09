d38ac72e26119357eff5da569d60d12b1087817c
Victoria <victoria.ad40@gmail.com>
2023-02-09 17:29:55 -0300

    add function history page

--- client/src/views/Comittees/FunctionHistory/index.tsx
+++ client/src/views/Comittees/FunctionHistory/index.tsx
@@ -1,22 +1,62 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
+import ExportableTable from '../../../components/ExportableTable'
 import HeaderSecondary from '../../../components/Header/HeaderSecondary'
+import Table from '../../../components/Table'
+import Title from '../../../components/Title'
 import { EntityContext } from '../../../context/CommitteeContext'
+import { createPDF } from '../../../utils/CreatePDF'
+import { committee_details_mock } from '../../../_mock/comittee'
 
 const FunctionHistory = () => {
   const { currentEntity } = useContext(EntityContext)
+  const [exportPDF, setExportPDF] = useState(false)
+  const table = useRef<HTMLInputElement>(null)
+  const [comitteeContent, setComitteeContent] = useState<any[]>([
+    ...committee_details_mock,
+  ])
+
+  useEffect(() => {
+    // TODO: use currentEntity.id or curentEntity.name to get the correct info
+
+    let content = [...committee_details_mock]
+    setComitteeContent(content)
+  }, [])
+
+  useEffect(() => {
+    if (exportPDF) {
+      try {
+        createPDF(table, 'committees')
+      } catch (e) {
+        console.log(e)
+      } finally {
+        setExportPDF(false)
+      }
+    }
+  }, [exportPDF])
 
   return (
     <>
-      <HeaderSecondary
-        headerTitle={`HISTÓRICO - ${currentEntity.name}`}
-        buttonType="export"
-        backButtonMsg="voltar às comissões"
-        handleExport={(type) => {
-          /* TODO */
-          console.log(type)
-        }}
-      />
-      Bora ver o histórico de um cargo: {currentEntity.id}, {currentEntity.name}
+      {exportPDF ? (
+        <div ref={table}>
+          <Title type="secondary"> HISTÓRICO - {currentEntity.name} </Title>
+          <ExportableTable
+            type={'committee-details'}
+            content={comitteeContent}
+          />
+        </div>
+      ) : (
+        <>
+          <HeaderSecondary
+            headerTitle={`HISTÓRICO - ${currentEntity.name}`}
+            buttonType="export"
+            backButtonMsg="voltar às comissões"
+            handleExport={(type) => {
+              type === 'pdf' && setExportPDF(true)
+            }}
+          />
+          <Table type={'committee-details'} content={comitteeContent} />
+        </>
+      )}
     </>
   )
 }
