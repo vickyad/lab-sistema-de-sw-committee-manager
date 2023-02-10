772e3f388e3c486036944d2f40e8811936639d8e
Vix <luc.dorneles@gmail.com>
2023-02-10 19:12:45 -0300

    Added RequestManager entity to handle all requests to backend access points.

---
+++ client/src/utils/RequestManager.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosInstance } from 'axios'
+import BackendPaths from '../constants/BackendPaths';
+
+class requestManager {
+
+    private axios_socket: AxiosInstance;
+
+    constructor() {
+        this.axios_socket = axios.create()
+    }
+
+    
+
+    private async makeGetRequest(access_point: string, params:any = {}) {
+        let result = await this.axios_socket.get(access_point, 
+            {params: params})
+            .then(res => res.data)
+            .catch(err => console.log(err))
+      
+        return result
+    }
+
+    async getAllMembers() {
+        return await this.makeGetRequest(BackendPaths.MEMBER_ALL)
+    }
+
+    async getMemberList() {
+        return await this.makeGetRequest(BackendPaths.MEMBER_OPTIONS)
+    }
+
+    async getAllCommittees() {
+        return await this.makeGetRequest(BackendPaths.COMMITTEE_ALL)
+    }
+
+    async getCommitteeList() {
+        return await this.makeGetRequest(BackendPaths.COMMITTEE_OPTIONS)
+    }
+
+    async getMembersOnCommitteeList() {
+        return await this.makeGetRequest(BackendPaths.MEMBER_ON_COMMITTEE_LIST)
+    }
+
+    async getMembersOnCommitteeDetails(member_id: string) {
+        let params={member_id: member_id}
+        return await this.makeGetRequest(BackendPaths.COMMITTEE_OPTIONS, params)
+    }
+}
+export default new requestManager()
