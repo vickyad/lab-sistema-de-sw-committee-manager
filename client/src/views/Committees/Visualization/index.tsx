fb8baefbdb5fec5b7a525b1a1875115528c66296
Vix <luc.dorneles@gmail.com>
2023-02-12 13:36:41 -0300

    Updated "Committees" screen to update based on backend database content. Both this and previous commit do not update the details of Members and Committees, that functionality is yet to be implemented.

--- client/src/views/Committees/Visualization/index.tsx
+++ client/src/views/Committees/Visualization/index.tsx
@@ -14,67 +14,22 @@ import { createPDF } from '../../../utils/CreatePDF'
 import { getEmptyEntity } from '../../../utils/EmptyEntity'
 import { committee_mock } from '../../../_mock/committee'
 import axios from 'axios'
-
-const axios_committee = axios.create({
-  baseURL: "http://localhost:3000/committee"
-})
-const axios_membersoncommittees = axios.create({
-  baseURL: "http://localhost:3000/member_on_committee"
-})
-
-async function fetch_committee_info() {
-  let result = await axios_committee.get("/all")
-      .then(res => res.data)
-      .catch(err => console.log(err))
-
-  return result
-}
-
-async function fetch_membersoncommittees_info() {
-  let result = await axios_membersoncommittees.get("/all")
-      .then(res => res.data)
-      .catch(err => console.log(err))
-
-  return result
-}
-
-function format_committee_info(committee_info: any[]) {
-  let formated_committee_info = [] as any[]
-
-  committee_info.forEach( (committee : any) => {
-    let formated_date_duration = "-"
-
-    if(committee.begin_date != null && committee.end_date != null){
-      formated_date_duration = committee.begin_date.substring(0, 9) + " a " + committee.end_date.substring(0, 9)
-    }
-
-    formated_committee_info.push({id: committee.id, 
-                              content: [committee.name, 
-                                committee.bond, 
-                                committee.ordinance, 
-                                formated_date_duration,
-                                committee.term + "°"]})
-  })
-
-  return formated_committee_info
-}
+import RequestManager from '../../../utils/RequestManager'
+import { formatCommittee } from '../../../utils/FormatUtils'
 
 const Visualization = () => {
   const [displayPopup, setDisplayPopup] = useState(false)
   const [searchtext, setSearchText] = useState('')
-  const [committeeContent, setCommitteeContent] = useState<any[]>([
-    ...committee_mock,
-  ])
-  const [displayedContent, setDisplayedContent] = useState<any[]>([
-    ...committee_mock,
-  ])
+  const [committeeContent, setCommitteeContent] = useState<any[]>([])
+  const [displayedContent, setDisplayedContent] = useState<any[]>([])
   const [exportPDF, setExportPDF] = useState(false)
   const table = useRef<HTMLInputElement>(null)
 
   async function fetch_and_format_committee_info()
   {
-    const committee_info = await fetch_committee_info()
-    const formated_committee_info = format_committee_info(committee_info)
+    const committee_info = await RequestManager.getAllCommittees()
+    const formated_committee_info = formatCommittee(committee_info)
+    console.log(formated_committee_info)
     setCommitteeContent(formated_committee_info)
     setDisplayedContent(formated_committee_info)
   }
@@ -116,7 +71,13 @@ const Visualization = () => {
   }, [exportPDF])
 
   useEffect(() => {
-    fetch_and_format_committee_info()
+    const request_answer =  async() =>  {
+      let committee_content = await RequestManager.getAllCommittees()
+      committee_content = formatCommittee(committee_content)
+      setCommitteeContent(committee_content)
+      setDisplayedContent(committee_content)
+    }
+    request_answer()
 
     if (action === 'search') {
       setSearchText(currentCommittee.name)
