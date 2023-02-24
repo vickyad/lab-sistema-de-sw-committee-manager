f8bc04520b2ef6b003c9c35435a5285b28efcfe9
Victoria <victoria.ad40@gmail.com>
2023-02-24 15:00:28 -0300

    add menu labels

--- client/src/components/NavBar/styles.ts
+++ client/src/components/NavBar/styles.ts
@@ -5,30 +5,38 @@ export const Container = styled.header`
   top: 0;
   left: 0;
   height: 100vh;
-  width: 5rem;
+  width: 13rem;
   background-color: #5085a5;
   padding: 2rem 0;
-  `
+`
 
 export const LinkContainer = styled.nav`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  padding-right: 1rem;
 `
 
 export const Link = styled.a<{ selected: boolean }>`
   background-color: transparent;
-  width: 3.75rem;
-  height: 3.75rem;
-  border-radius: 0.625rem;
+  width: -webkit-fill-available;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+  column-gap: 1.2rem;
+  padding: 0.6rem 1rem;
   margin: 0.5rem 0;
+  text-decoration: none;
+  border-radius: 0 2rem 2rem 0;
 
   ${({ selected }) =>
     selected &&
     css`
       background-color: #ffffff33;
     `}
+`
+
+export const Label = styled.p`
+  font-size: 0.813rem;
+  color: #ffffff;
+  font-weight: 500;
 `
